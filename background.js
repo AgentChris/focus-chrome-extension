7cdc087a6d7a3a037cc44d51813bc4bf944e27d3
Cristian Poputea <poputeacristi@gmail.com>
2022-04-20 18:40:22 +0300

    feat: add support for instagram and facebook

--- background.js
+++ background.js
@@ -1,14 +1,31 @@
+const bannedWebsites = ['www.youtube.com', 'www.facebook', 'www.instagram.com'];
 async function closeYoutubeTabs() {
     let { numberYoutubeClosedTabs } = await chrome.storage.local.get('numberYoutubeClosedTabs');
+    let { numberInstagramClosedTabs } = await chrome.storage.local.get('numberInstagramClosedTabs');
+    let { numberFacebookClosedTabs } = await chrome.storage.local.get('numberFacebookClosedTabs');
 
     if (!numberYoutubeClosedTabs) {
         numberYoutubeClosedTabs = 0;
     }
+    if (!numberInstagramClosedTabs) {
+        numberInstagramClosedTabs = 0;
+    }
+    if (!numberFacebookClosedTabs) {
+        numberFacebookClosedTabs = 0;
+    }
     const tabs = await chrome.tabs.query({});
     tabs.forEach((tab)=>{
-        if (tab.url.indexOf('youtube') > 0){
+        if (bannedWebsites.some(v => tab.url.includes(v))){
             chrome.tabs.remove(tab.id);
-            chrome.storage.local.set({"numberYoutubeClosedTabs": numberYoutubeClosedTabs + 1});
+            if (tab.url.indexOf(bannedWebsites[0]) > 0 ) {
+                chrome.storage.local.set({"numberYoutubeClosedTabs": numberYoutubeClosedTabs + 1});
+            }
+            if (tab.url.indexOf(bannedWebsites[1]) > 0 ) {
+                chrome.storage.local.set({"numberFacebookClosedTabs": numberFacebookClosedTabs + 1});
+            }
+            if (tab.url.indexOf(bannedWebsites[2]) > 0 ) {
+                chrome.storage.local.set({"numberInstagramClosedTabs": numberInstagramClosedTabs + 1});
+            }
         }
     })
 }
--- script.js
+++ script.js
@@ -2,3 +2,11 @@ chrome.storage.local.get('numberYoutubeClosedTabs', function (result) {
     const info = document.getElementById('numberYoutubeClosedTabs');
     info.innerText = result.numberYoutubeClosedTabs || 0;
 });
+chrome.storage.local.get('numberFacebookClosedTabs', function (result) {
+    const info = document.getElementById('numberFacebookClosedTabs');
+    info.innerText = result.numberFacebookClosedTabs || 0;
+});
+chrome.storage.local.get('numberInstagramClosedTabs', function (result) {
+    const info = document.getElementById('numberInstagramClosedTabs');
+    info.innerText = result.numberInstagramClosedTabs || 0;
+});
